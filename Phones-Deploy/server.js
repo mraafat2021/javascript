ba14d0a423e299e26c01d7c0785332fd54b1c8be
mraafat2021 <87667833+mraafat2021@users.noreply.github.com>
2021-10-21 21:49:48 +0200

    Add files via upload

---
+++ Phones-Deploy/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(process.env.PORT || 2000);
