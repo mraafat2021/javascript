3cdb49b293c03c77c19f81a8e51eb77529873a3e
mraafat2021 <87667833+mraafat2021@users.noreply.github.com>
2021-10-21 21:55:25 +0200

    Update server.js

--- Phones-Deploy/server.js
+++ Phones-Deploy/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) =>{
-  
+  res.sendFile(path.join(__dirname, "public/index.html"))
 })
 
 app.listen(process.env.PORT || 2000);
